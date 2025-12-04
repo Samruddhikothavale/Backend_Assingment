5afa1c8d92d8888094c8eadd15337a4c719a80d4
Samruddhikothavale <168101139+Samruddhikothavale@users.noreply.github.com>
2025-12-04 21:54:14 +0530

    Update Dashboard.jsx

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 require("dotenv").config();
 
-const API_BASE = `${process.env.SERVER_URL}`;
+const API_BASE = `${import.meta.env.VITE_SERVER_URL}/task/dashboard`;
 
 const Dashboard = () => {
     const navigate = useNavigate();
